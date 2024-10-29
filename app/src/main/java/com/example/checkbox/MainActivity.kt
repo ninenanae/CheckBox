c76ffff4a3528e4251375dff093a1796d9ac3611
ninenanae <1010.docx@gmail.com>
2024-10-29 19:29:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/checkbox/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.checkbox
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.checkbox.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val tvChos : TextView = findViewById(R.id.tvChoose)
+
+        binding.btnChoose.setOnClickListener {
+             val result = StringBuilder()
+            if (binding.cbCar.isChecked) {
+                tvChos.text = binding.cbCar.text
+            } else if (binding.cbBus.isChecked){
+                tvChos.text = binding.cbBus.text
+            }
+        }
+
+    }
+}
